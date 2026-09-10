ae6f3e8ca5e9e8dbd411fb9a69dbc18801659e53
orellanes <orellanescarlos1966@yahoo.com>
2026-09-09 20:50:57 -0400

    feat: add NurseTrack Enterprise application entrypoint

---
+++ nursetrack-enterprise/src/main/kotlin/com/nursetrack/enterprise/NurseTrackEnterpriseApplication.kt
@@ -0,0 +1,11 @@
+package com.nursetrack.enterprise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NurseTrackEnterpriseApplication
+
+fun main(args: Array<String>) {
+    runApplication<NurseTrackEnterpriseApplication>(*args)
+}
