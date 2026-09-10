dd7b0856a38246ace29765984b9fe16e55bbdc0a
orellanes <orellanescarlos1966@yahoo.com>
2026-09-09 22:22:07 -0400

    feat(enterprise): add PHQ-9 repository

---
+++ nursetrack-enterprise/src/main/kotlin/com/nursetrack/enterprise/phq9/Phq9Repository.kt
@@ -0,0 +1,8 @@
+package com.nursetrack.enterprise.phq9
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface Phq9Repository : JpaRepository<Phq9Assessment, UUID> {
+    fun findAllByCompanyIdAndPatientIdOrderByScreeningDateDescCreatedAtDesc(companyId: UUID, patientId: UUID): List<Phq9Assessment>
+}
